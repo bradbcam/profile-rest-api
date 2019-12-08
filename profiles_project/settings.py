248da4ff0e164dba117417096951ebc4ce6cacfc
Brad <haxxman@yahoo.com>
2019-12-08 20:04:05 +0000

    added allowed hosts for Ec2

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '*!o8qw_5jp-km(tbe9upe3@a#dv++*&+ik4f2iqb84mx%$81+5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-35-178-177-168.eu-west-2.compute.amazonaws.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
